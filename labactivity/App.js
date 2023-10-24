d27a3d388cf27803212b6bc6bca9e67ae8209006
kinzakhatoon <145096789+kinzakhatoon@users.noreply.github.com>
2023-10-24 09:34:07 -0700

    Add files via upload

--- labactivity/App.js
+++ labactivity/App.js
@@ -1,43 +1,73 @@
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-  const handleIncrement = () => {
-    setCount(count + 1);
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
   };
 
-  const handleDecrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.counterText}>Count: {count}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="Increment" onPress={handleIncrement} color="green" />
-        <Button title="Decrement" onPress={handleDecrement} color="green" />
+      <Text style={styles.header}>To-Do List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a task"
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <Button title="Add" onPress={addTask} />
       </View>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.taskItem}>
+            <Text>{item}</Text>
+            <Button title="Remove" onPress={() => removeTask(index)} />
+          </View>
+        )}
+      />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
   },
-  counterText: {
+  header: {
     fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
-  buttonContainer: {
+  inputContainer: {
     flexDirection: 'row',
-    marginTop: 20,
+    marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    padding: 10,
+    marginRight: 10,
+  },
+  taskItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
   },
 });
-
-export default App;
