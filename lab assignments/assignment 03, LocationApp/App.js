f0fb0ddc9a3d70dad27913693d0ce2c86d7abb7e
kinzakhatoon <145096789+kinzakhatoon@users.noreply.github.com>
2023-12-16 01:10:55 -0800

    lab assignment

---
+++ lab assignments/assignment 03, LocationApp/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+export default function App() {
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    // Get user's location when the component is loaded
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {userLocation ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: userLocation.latitude,
+            longitude: userLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {/* Marker for user's location */}
+          <Marker
+            coordinate={{
+              latitude: userLocation.latitude,
+              longitude: userLocation.longitude,
+            }}
+            title="User's Location"
+            description="You are here!"
+          />
+
+          {/* Marker for COMSATS Attock */}
+          <Marker
+            coordinate={{
+              latitude: 33.7833373941881,
+              longitude: 72.35347025189913,
+            }}
+            title="COMSATS Attock"
+            description="Welcome to COMSATS Attock!"
+          />
+        </MapView>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
