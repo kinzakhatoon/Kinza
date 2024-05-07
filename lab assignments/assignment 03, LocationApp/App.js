3dd9b180cfe907969d49150a8a5431b297694803
kinzakhatoon <145096789+kinzakhatoon@users.noreply.github.com>
2024-05-07 00:00:55 -0700

    Update App.js

--- lab assignments/assignment 03, LocationApp/App.js
+++ lab assignments/assignment 03, LocationApp/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
-
+import *;
 export default function App() {
   const [userLocation, setUserLocation] = useState(null);
 
